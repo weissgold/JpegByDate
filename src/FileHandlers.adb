676be20bb03a2d37f46c156d44a9bddb4c605e1e
unknown <rauch.lukas-tit14@ET116.it.ba-ravensburg.de>
2016-11-03 18:16:38 +0100

    Datei einlesen

--- src/FileHandlers.adb
+++ src/FileHandlers.adb
@@ -1,5 +1,8 @@
 -- Verwendete Packages
---> NONE
+with Ada.Directories;
+with Ada.Direct_IO;
+with Ada.Exceptions;
+with Ada.Strings.Unbounded;
 
 -- Package für FileHandler
 package body FileHandlers is
@@ -13,12 +16,48 @@ package body FileHandlers is
       return handler;
    end create;
 
+   -- Datei einlesen
+   -- Rückgabe -1 bei fehlender oder leerer Datei
+   function readFile(name: String; buffer: out Ada.Strings.Unbounded.Unbounded_String) return Integer is
+   begin
+      declare
+         -- Dateigröße auslesen und Einlesen vorbereiten
+         size: Ada.Directories.File_Size := Ada.Directories.Size(name);
+         subtype File_String is String(1..Integer(size));
+         package File_String_IO is new Ada.Direct_IO(File_String);
+         file: File_String_IO.File_Type;
+         buf: File_String;
+      begin
+         -- Datei einlesen
+         File_String_IO.open(file, File_String_IO.In_File, name);
+         File_String_IO.Read(file, buf);
+         File_String_IO.Close(file);
+         Ada.Strings.Unbounded.Append(buffer, buf);
+         return Integer(size);
+      end;
+
+   exception
+      -- Im Fehlerfall Dateigröße -1 zurückgeben
+      when E: others =>
+         return -1;
+   end readFile;
+
    -- Dateien verarbeiten
    procedure exec(This: access FileHandler; output: access Outputs.Output'Class) is
+      str: Ada.Strings.Unbounded.Unbounded_String;
+      buffer: Ada.Strings.Unbounded.Unbounded_String;
+      size: Integer;
    begin
       -- Ausgabe aller Dateien in der Liste
       while This.all.files.hasNext loop
-         output.display(This.all.files.next);
+         -- Datei einlesen
+         str := Ada.Strings.Unbounded.Null_Unbounded_String;
+         str := Ada.Strings.Unbounded.To_Unbounded_String(This.all.files.next);
+         size := readFile(Ada.Strings.Unbounded.To_String(str), buffer);
+
+         -- Dateiname & Größe ausgeben
+         output.display(Ada.Strings.Unbounded.To_String(str));
+         output.display(Integer'Image(size));
       end loop;
    end exec;
 
